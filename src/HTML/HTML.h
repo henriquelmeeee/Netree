e6c9225df68784a04de858c1e07de5f9474768fe
Henriquelmeeee <smtpwebhost@gmail.com>
2023-10-08 19:18:51 -0300

    HTML: HTML.h: TODO mark created

---
+++ src/HTML/HTML.h
@@ -0,0 +1,7 @@
+// Henrique:
+// TODO
+// This file will create the interface between main.cpp of Browser/
+// and the HTML lexer and parser
+// the Javascript engine will be executed by the DOM 
+// and the 'Program' class of the Javascript will have an DOM-node pointer
+// which points to the current DOM node that can be modified by the Javascript.
