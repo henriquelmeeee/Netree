dba35ca2dd517436521fd1a42fdb230a84cae5ab
Henriquelmeeee <smtpwebhost@gmail.com>
2023-10-08 18:01:14 -0300

    JS: Creating first nodes

--- src/JS/AST/Nodes.h
+++ src/JS/AST/Nodes.h
@@ -4,3 +4,38 @@
 */
 
 #pragma once
+
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+
+#include "Variable.h"
+
+class Program {
+
+};
+
+class Scope : public Program {
+  private:
+    std::vector<Variable> m_variables;
+  public:
+    Scope() { std::cout << "Scope::Scope() TODO"; exit(1); }
+};
+
+class ASTNode : public Scope {
+  public:
+    const std::string class_name() { return "ASTNode"; }
+
+    ASTNode() {
+      std::cout << "ASTNode::ASTNode(): Generic ASTNode cannot be constructed.";
+      exit(1);
+    }
+
+    virtual void execute(Scope&) {
+      std::cout << "ASTNode::execute(): Generic ASTNode cannot be executed.";
+      exit(1);
+    }
+
+};
---
+++ src/JS/AST/Variable.h
@@ -0,0 +1,15 @@
+/*
+ Copyright (c) 2023 [Henrique] [Zing]
+ SPDX-License-Identifier: BSD-2-Clause
+*/
+
+#pragma once
+
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <iostream>
+
+class Variable {
+
+};
