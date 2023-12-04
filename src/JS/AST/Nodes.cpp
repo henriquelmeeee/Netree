9138f1d76182230ae1d9d24686268e94c4056170
Henriquelmeeee <smtpwebhost@gmail.com>
2023-12-04 15:56:04 -0300

    JS: execute() methods for MemberExpression (starting)

--- src/JS/AST/Nodes.cpp
+++ src/JS/AST/Nodes.cpp
@@ -4,13 +4,25 @@
 */
 
 #include "Nodes.h"
+#include <string.h>
 
 namespace JS {
 /* execute() methods */
 
-Value VariableDeclaration::execute(Scope* scope) {
+Value VariableDeclaration::execute(Scope* current_scope) {
   std::cout << "VariableDeclaration::execute(): called\n";
-  return m_variable->execute(scope);
+  return m_variable->execute(current_scope);
 }
 
+Value MemberExpression::execute(Scope* current_scope) {
+  std::cout << "MemberExpression::execute(): called\n";
+  if(strcmp("Object", m_lhs->class_name()).as_string() == 0) {
+    std::cout << "\tIt's a object\n";
+    // TODO
+    return Value(Null);
+  }
+
+  return Value(Null);
 }
+
+} // namespace JS
