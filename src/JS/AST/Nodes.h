3e0000cca3697ded5b79b0bd6b47907e507f5254
Henriquelmeeee <smtpwebhost@gmail.com>
2023-12-04 15:47:03 -0300

    JS: Adding more AST Nodes

--- src/JS/AST/Nodes.h
+++ src/JS/AST/Nodes.h
@@ -12,6 +12,7 @@
 #include <vector>
 
 #include "AST.h"
+#include "Value.h"
 
 namespace JS {
 
@@ -22,13 +23,23 @@ class Program {
 class Scope : public Program {
   private:
     std::vector<Variable*> m_variables;
+    Scope* m_previous = nullptr;
   public:
-    Scope() { std::cout << "Scope::Scope() TODO"; exit(1); }
+    
+    Scope(Scope* previous = nullptr) : m_previous(previous) {
+      std::cout << "Scope::Scope() TODO"; exit(1); 
+    }
+
+    ~Scope() {
+      free(this);
+    }
 };
+
+#define CLASS_NAME(name) virtual const std::string class_name() { return name; }
 
 class ASTNode : public Scope {
   public:
-    virtual const std::string class_name() { return "ASTNode"; }
+    CLASS_NAME("ASTNode")
 
     ASTNode() {
       std::cout << "ASTNode::ASTNode(): Generic ASTNode cannot be constructed.";
@@ -42,16 +53,71 @@ class ASTNode : public Scope {
 
 };
 
+/* Declarations */
+
 class VariableDeclaration : public ASTNode {
   private:
     std::string m_name; // var name
     Variable* m_variable;
   public:
-    virtual const std::string class_name() { return "VariableDeclaration"; }
+    CLASS_NAME("VariableDeclaration")
 
     VariableDeclaration(std::string& name, Variable* variable) : m_name(name), m_variable(variable) {}
 
     virtual Value execute(Scope*);
 };
+
+class FunctionDeclaration : public ASTNode {
+  public:
+    std::vector<ASTNode*> m_ast_nodes;
+
+    CLASS_NAME("FunctionDeclaration")
+
+    virtual Value execute(Scope* current_scope) {
+      // TODO pass it into 'Nodes.cpp'
+      Scope* new_scope = new Scope(current_scope);
+      for(ASTNode* node : m_ast_nodes) {
+        Value returned = node->execute(new_scope);
+        if(returned.from_return_instruction()) {
+          delete new_scope;
+          return returned;
+        }
+      }
+      delete new_scope;
+      return Value(Null);
+    }
+};
+
+/* Expressions */
+
+class ExpressionStatement : public ASTNode {
+  private:
+    ASTNode* m_expression;
+  public:
+    CLASS_NAME("ExpressionStatement")
+    virtual Value execute(Scope*);
+};
+
+class MemberExpression : public ASTNode {
+  private:
+    ASTNode* m_lhs;
+    ASTNode* m_rhs;
+  public:
+    CLASS_NAME("MemberExpression")
+
+    virtual Value execute(Scope*);
+};
+
+class CallExpression : public ASTNode {
+  private:
+    ASTNode* m_function;
+    std::vector<ASTNode*> m_arguments;
+  public:
+    CLASS_NAME("ClassExpression")
+
+    virtual Value execute(Scope*);
+};
+
+
 
 }
